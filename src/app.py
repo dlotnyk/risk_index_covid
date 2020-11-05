d7e98de7075e782cb63c6166273eba2eafe5193c
dlotnyk_ubuntu_vm <dmytro.lotnyk@gmail.com>
2020-11-05 22:48:06 +0100

    some logs

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import threading
 import time
 from wsgiref.simple_server import make_server
 from flask import Flask, render_template, url_for
+from logger import log_settings
+
+app_log = log_settings()
 
 
 class WsgiRefServer(threading.Thread):
@@ -11,9 +14,11 @@ class WsgiRefServer(threading.Thread):
         self._server = make_server(host, port, wsgi_app)
 
     def run(self):
+        app_log.debug("WSGI server starts")
         self._server.serve_forever(poll_interval=0.5)
 
     def stop(self):
+        app_log.debug("WSGI server stops")
         self._server.shutdown()
         self.join()
 
@@ -24,13 +29,16 @@ def get_app():
     @flask_app.route("/")
     @flask_app.route("/home")
     def home():
+        app_log.debug("Route `home` is called")
         return render_template("home.html")
 
     return flask_app
 
 
 if __name__ == "__main__":
+    app_log.info("flask app starts")
     http_thread = WsgiRefServer(get_app())
     http_thread.start()
     time.sleep(60)
     http_thread.stop()
+    app_log.info("flask app stops")
